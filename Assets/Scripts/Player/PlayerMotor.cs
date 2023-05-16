97b71b4cf3d657480927fb1166f339385af8e2a3
Hyago Oliveira <hyagogow@gmail.com>
2023-05-16 13:59:22 -0300

    Refactor Motor

--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -3,29 +3,30 @@ using UnityEngine;
 namespace Flux.EvaluationProject
 {
     /// <summary>
-    /// Component responsible to deal only with the Player physics using a local <see cref="CharacterController"/>.
+    /// Component responsible to deal with the Player physics using a local <see cref="CharacterController"/>.
     /// </summary>
+    [RequireComponent(typeof(PlayerAnimator))]
     [RequireComponent(typeof(CharacterController))]
     public class PlayerMotor : MonoBehaviour
     {
+        [SerializeField] private PlayerAnimator animator;
         [SerializeField] private CharacterController controller;
 
         [Header("Movement")]
         [Tooltip("Move speed of the character in m/s"), Min(0f)]
-        public float moveSpeed = 2.0f;
+        public float moveSpeed = 2f;
         [Tooltip("Sprint speed of the character in m/s"), Min(0f)]
-        public float sprintSpeed = 5.335f;
-        [Tooltip("How fast the character turns to face movement direction")]
-        [Range(0.0f, 0.3f)]
-        public float rotationSmoothTime = 0.12f;
-        [Tooltip("Acceleration and deceleration"), Min(0f)]
-        public float speedChangeRate = 10.0f;
+        public float sprintSpeed = 6f;
+        [Tooltip("Acceleration rate for movement"), Range(0f, 1f)]
+        public float moveAcceleration = 0.2f;
+        [Tooltip("Acceleration rate for turning"), Range(0.1f, 1f)]
+        public float turnAcceleration = 0.6f;
 
         [Header("Jump")]
         [Tooltip("The height the player can jump"), Min(0f)]
         public float jumpHeight = 1.2f;
         [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
-        public float gravity = -15.0f;
+        public float gravity = -9.8f;
 
         [Header("Ground")]
         [Tooltip("Useful for rough ground")]
@@ -35,113 +36,152 @@ namespace Flux.EvaluationProject
         [Tooltip("What layers the character uses as ground")]
         public LayerMask groundLayers;
 
-        /// <summary>
-        /// If the character is grounded or not. Not part of the CharacterController built in grounded check.
-        /// </summary>
+        public bool CanMove { get; set; } = true;
         public bool IsGrounded { get; private set; }
+        public bool IsSprinting { get; private set; }
+        public bool IsMoveInputting { get; private set; }
 
-        public bool IsSprinting { get; set; }
+        public Vector2 MoveInput { get; private set; }
 
-        public float Speed { get; private set; }
+        public Vector3 Velocity { get; private set; }
 
-        private Camera mainCamera;
-        private float targetRotation;
-        private float rotationVelocity;
-        private float verticalSpeed;
+        public float VerticalSpeed { get; private set; }
 
-        private const float terminalVelocity = 53F;
-        private const float groundedVerticalSpeed = -2F;
+        private Transform mainCamera;
+        private Vector3 moveDirection;
+        private Vector3 currentTurningVelocity;
+        private float currentMoveSpeed;
 
-        private void Reset() => controller = GetComponent<CharacterController>();
+        private void Reset()
+        {
+            animator = GetComponent<PlayerAnimator>();
+            controller = GetComponent<CharacterController>();
+        }
 
-        private void Awake() => mainCamera = Camera.main;
+        private void Awake()
+        {
+            mainCamera = Camera.main.transform;
+            UpdateGroundCollision();
+        }
 
         private void Update()
         {
-            CheckGround();
-            UpdateVerticalSpeed();
+            UpdateMovement();
+            UpdateRotation();
+            UpdateGroundCollision();
+            UpdateAnimator();
         }
 
-        public void Move(Vector2 input)
+        public void SetSprintInput(bool isSprinting) => IsSprinting = CanMove && isSprinting;
+
+        public void SetMoveInput(Vector2 input)
         {
-            bool hasInput = Mathf.Abs(input.sqrMagnitude) > 0f;
-            float targetSpeed = hasInput ? GetMoveSpeed() : 0F;
-
-            // a reference to the players current horizontal velocity
-            float currentHorizontalSpeed = new Vector3(controller.velocity.x, 0.0f, controller.velocity.z).magnitude;
-
-            float speedOffset = 0.1f;
-            float inputMagnitude = input.magnitude;
-
-            // accelerate or decelerate to target speed
-            if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
-            {
-                // creates curved result rather than a linear one giving a more organic speed change
-                // note T in Lerp is clamped, so we don't need to clamp our speed
-                Speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * speedChangeRate);
-
-                // round speed to 3 decimal places
-                Speed = Mathf.Round(Speed * 1000f) / 1000f;
-            }
-            else
-            {
-                Speed = targetSpeed;
-            }
-
-            //animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
-
-            // normalise input direction
-            Vector3 inputDirection = new Vector3(input.x, 0.0f, input.y).normalized;
-
-            if (hasInput)
-            {
-                targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
-                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity, rotationSmoothTime);
-
-                // rotate to face input direction relative to camera position
-                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
-            }
-
-
-            Vector3 targetDirection = (Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward).normalized;
-
-            // move the player
-            var speedPerSeconds = Speed * Time.deltaTime;
-            var verticalVelocityPerSeconds = Vector3.up * verticalSpeed * Time.deltaTime;
-            var velocity = targetDirection * speedPerSeconds + verticalVelocityPerSeconds;
-
-            controller.Move(velocity);
+            MoveInput = CanMove ? input : Vector2.zero;
+            IsMoveInputting = Mathf.Abs(MoveInput.sqrMagnitude) > 0F;
         }
 
-        private void CheckGround()
+        public void StopMoveInput()
         {
-            // set sphere position, with offset
-            var spherePosition = new Vector3(transform.position.x, transform.position.y - groundedOffset, transform.position.z);
+            MoveInput = Vector2.zero;
+            IsMoveInputting = false;
+        }
+
+        public void StopVelocity() => Velocity = Vector3.zero;
+
+        private void UpdateMovement()
+        {
+            UpdateMoveDirection();
+            AddGravityIntoVerticalSpeed();
+            UpdateCurrentMoveSpeed();
+
+            Velocity = currentMoveSpeed * moveDirection + Vector3.up * VerticalSpeed;
+
+            var velocityPerSecond = Velocity * Time.deltaTime;
+            controller.Move(velocityPerSecond);
+        }
+
+        private void UpdateMoveDirection()
+        {
+            moveDirection = IsMoveInputting ?
+                GetMoveInputDirectionRelativeToCamera() :
+                Vector3.zero;
+        }
+
+        private void AddGravityIntoVerticalSpeed()
+        {
+            const float maxVerticalSpeed = -25F;
+            VerticalSpeed += gravity * Time.deltaTime;
+            if (VerticalSpeed < maxVerticalSpeed) VerticalSpeed = maxVerticalSpeed;
+        }
+
+        private void UpdateCurrentMoveSpeed()
+        {
+            if (!IsMoveInputting)
+            {
+                currentMoveSpeed = 0f;
+                return;
+            }
+
+            var targetSpeed = IsSprinting ? sprintSpeed : moveSpeed;
+
+            currentMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, targetSpeed, moveAcceleration);
+            currentMoveSpeed = RoundInto3DecimalPlaces(currentMoveSpeed);
+        }
+
+        private void UpdateRotation()
+        {
+            var currentDirection = transform.position + transform.forward;
+            var targetDirection = transform.position + moveDirection;
+            var direction = Vector3.MoveTowards(currentDirection, targetDirection, turnAcceleration);
+
+            transform.LookAt(direction);
+        }
+
+        private void UpdateGroundCollision()
+        {
+            var spherePosition = transform.position + Vector3.down * groundedOffset;
             IsGrounded = Physics.CheckSphere(spherePosition, groundedRadius, groundLayers, QueryTriggerInteraction.Ignore);
         }
 
-        private void UpdateVerticalSpeed()
+        private void UpdateAnimator()
         {
-            if (IsGrounded)
-            {
-                // stop our velocity dropping infinitely when grounded
-                if (verticalSpeed < 0.0f)
-                {
-                    verticalSpeed = groundedVerticalSpeed;
-                }
-            }
-            else if (verticalSpeed < terminalVelocity)
-            {
-                verticalSpeed += gravity * Time.deltaTime;
-            }
+            animator.SetGrounded(IsGrounded);
+            animator.SetNormalizedSpeed(GetNormalizedSpeed());
         }
 
-        private void Jump()
+        private Vector3 GetMoveInputDirectionRelativeToCamera()
         {
+            var right = mainCamera.right;
+            right.y = 0f;
+            var forward = Vector3.Cross(right, Vector3.up);
+            return (right * MoveInput.x + forward * MoveInput.y).normalized;
+        }
+
+        public void Jump()
+        {
+            if (!IsAbleToJump()) return;
+
             // the square root of H * -2 * G = how much velocity needed to reach desired height
-            verticalSpeed = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            VerticalSpeed = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator.Jump();
         }
 
-        private float GetMoveSpeed() => IsSprinting ? sprintSpeed : moveSpeed;
+        public void Punch()
+        {
+            StopVelocity();
+            animator.Punch();
+        }
+
+        public void Kick()
+        {
+            StopVelocity();
+            animator.Kick();
+        }
+
+        private bool IsAbleToJump() => IsGrounded;
+
+        private float GetNormalizedSpeed() => currentMoveSpeed / sprintSpeed;
+
+        private static float RoundInto3DecimalPlaces(float value) => Mathf.Round(value * 1000f) / 1000f;
     }
 }
