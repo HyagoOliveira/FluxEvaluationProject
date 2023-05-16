91908061a5969d98c61779ead606bb59d007f37b
Hyago Oliveira <hyagogow@gmail.com>
2023-05-16 18:39:38 -0300

    Fix ResourcesLoad component

--- Assets/Scripts/ResourcesLoad.cs
+++ Assets/Scripts/ResourcesLoad.cs
@@ -4,16 +4,14 @@ namespace Flux.EvaluationProject
 {
     public class ResourcesLoad : MonoBehaviour
     {
-#if UNITY_EDITOR
         [SerializeField] private GameObject prefab;
-#endif
-        [SerializeField, HideInInspector] private string path;
 
         void Awake()
         {
-            path = prefab.name;
-            if (path != "")
-                Instantiate(Resources.Load(path), transform);
+            if (prefab == null) return;
+
+            var path = prefab.name;
+            Instantiate(Resources.Load(path), transform);
         }
     }
 }
