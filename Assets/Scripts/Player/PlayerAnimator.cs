94f96a3ff6e08c0c9b48deac8bf815f1a9749263
Hyago Oliveira <hyagogow@gmail.com>
2023-05-16 12:14:19 -0300

    Remove MotionSpeed animation param

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -15,12 +15,10 @@ namespace Flux.EvaluationProject
         private static readonly int speedId = Animator.StringToHash("Speed");
         private static readonly int groundedId = Animator.StringToHash("Grounded");
         private static readonly int jumpId = Animator.StringToHash("Jump");
-        private static readonly int motionSpeedId = Animator.StringToHash("MotionSpeed");
 
         private void Reset() => animator = GetComponent<Animator>();
 
         public void SetSpeed(float speed) => animator.SetFloat(speedId, speed);
-        public void SetMotionSpeed(float speed) => animator.SetFloat(motionSpeedId, speed);
         public void SetGrounded(bool isGrounded) => animator.SetBool(groundedId, isGrounded);
 
         public void Jump() => animator.SetTrigger(jumpId);
