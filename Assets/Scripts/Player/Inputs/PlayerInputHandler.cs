760090eefe9d6a8aa36eeb55b75797556bf53f43
Hyago Oliveira <hyagogow@gmail.com>
2023-05-16 14:58:02 -0300

    Refact PlayerInputHandler component

--- Assets/Scripts/Player/Inputs/PlayerInputHandler.cs
+++ Assets/Scripts/Player/Inputs/PlayerInputHandler.cs
@@ -5,23 +5,16 @@ using static Flux.EvaluationProject.PlayerInputActions;
 namespace Flux.EvaluationProject
 {
     /// <summary>
-    /// Component responsible to receive the Player inputs and forwards them.
+    /// Component responsible to receive the inputs and forward them into <see cref="PlayerMotor"/>.
     /// </summary>
-    [DefaultExecutionOrder(-1)]
     [RequireComponent(typeof(PlayerMotor))]
-    [RequireComponent(typeof(PlayerAnimator))]
     public class PlayerInputHandler : MonoBehaviour
     {
         [SerializeField] private PlayerMotor motor;
-        [SerializeField] private PlayerAnimator animator;
 
         private PlayerActions actions;
 
-        private void Reset()
-        {
-            motor = GetComponent<PlayerMotor>();
-            animator = GetComponent<PlayerAnimator>();
-        }
+        private void Reset() => motor = GetComponent<PlayerMotor>();
 
         private void Awake()
         {
@@ -29,25 +22,48 @@ namespace Flux.EvaluationProject
             actions = input.Player;
         }
 
-        private void OnEnable() => actions.Enable();
-
-        private void Update()
+        private void OnEnable()
         {
-            var moveInput = actions.Move.ReadValue<Vector2>();
-            var lookInput = actions.Look.ReadValue<Vector2>();
-            var jumpInput = IsButtonPressed(actions.Jump);
-            var sprintInput = IsButtonPressed(actions.Sprint);
-            var punchAttackInput = IsButtonPressed(actions.PunchAttack);
-            var kickAttackInput = IsButtonPressed(actions.KickAttack);
+            actions.Move.started += HandleMovePerformed;
+            actions.Move.performed += HandleMovePerformed;
+            actions.Move.canceled += HandleMoveCanceled;
 
-            motor.Move(moveInput);
-            motor.IsSprinting = sprintInput;
+            actions.Jump.started += HandleJumpStarted;
+            actions.PunchAttack.started += HandlePunchAttackStarted;
+            actions.KickAttack.started += HandleKickAttackStarted;
 
+            actions.Sprint.started += HandleSprintStarted;
+            actions.Sprint.canceled += HandleSprintCanceled;
+
+            actions.Enable();
         }
 
-        private void OnDisable() => actions.Disable();
+        private void OnDisable()
+        {
+            actions.Disable();
 
-        //Function InputAction.IsPressed() is only available on Input System 1.1.0
-        private static bool IsButtonPressed(InputAction button) => button.ReadValue<float>() > 0F;
+            actions.Move.started -= HandleMovePerformed;
+            actions.Move.performed -= HandleMovePerformed;
+            actions.Move.canceled -= HandleMoveCanceled;
+
+            actions.Jump.started -= HandleJumpStarted;
+            actions.PunchAttack.started -= HandlePunchAttackStarted;
+            actions.KickAttack.started -= HandleKickAttackStarted;
+
+            actions.Sprint.started -= HandleSprintStarted;
+            actions.Sprint.canceled -= HandleSprintCanceled;
+        }
+
+        private void HandleMovePerformed(InputAction.CallbackContext ctx) =>
+            motor.SetMoveInput(ctx.ReadValue<Vector2>());
+        private void HandleMoveCanceled(InputAction.CallbackContext _) =>
+            motor.SetMoveInput(Vector2.zero);
+
+        private void HandleJumpStarted(InputAction.CallbackContext _) => motor.Jump();
+        private void HandlePunchAttackStarted(InputAction.CallbackContext _) => motor.Punch();
+        private void HandleKickAttackStarted(InputAction.CallbackContext _) => motor.Kick();
+
+        private void HandleSprintStarted(InputAction.CallbackContext _) => motor.SetSprintInput(true);
+        private void HandleSprintCanceled(InputAction.CallbackContext _) => motor.SetSprintInput(false);
     }
 }
