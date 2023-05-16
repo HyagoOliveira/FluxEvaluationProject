b711c840761ed9129a595618829acd88fe1b8c4f
Hyago Oliveira <hyagogow@gmail.com>
2023-05-16 16:58:38 -0300

    Revert "Feed motor information"
    This reverts commit 4f01ebd1099369030157178550a52c214ab72d49.

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -6,11 +6,9 @@ namespace Flux.EvaluationProject
     /// Component responsible to deal only with the Player animations.
     /// </summary>
     [RequireComponent(typeof(Animator))]
-    [RequireComponent(typeof(PlayerMotor))]
     public class PlayerAnimator : MonoBehaviour
     {
         [SerializeField] private Animator animator;
-        [SerializeField] private PlayerMotor motor;
 
         private static readonly int punchId = Animator.StringToHash("Punch");
         private static readonly int kickId = Animator.StringToHash("Kick");
@@ -18,33 +16,7 @@ namespace Flux.EvaluationProject
         private static readonly int groundedId = Animator.StringToHash("Grounded");
         private static readonly int jumpId = Animator.StringToHash("Jump");
 
-        private void Reset()
-        {
-            animator = GetComponent<Animator>();
-            motor = GetComponent<PlayerMotor>();
-        }
-
-        private void Start() => SetGrounded(motor.IsGrounded);
-
-        private void Update()
-        {
-            SetGrounded(motor.IsGrounded);
-            SetNormalizedSpeed(motor.GetNormalizedSpeed());
-        }
-
-        private void OnEnable()
-        {
-            motor.OnJump += Jump;
-            motor.OnKick += Kick;
-            motor.OnPunch += Punch;
-        }
-
-        private void OnDisable()
-        {
-            motor.OnJump -= Jump;
-            motor.OnKick -= Kick;
-            motor.OnPunch -= Punch;
-        }
+        private void Reset() => animator = GetComponent<Animator>();
 
         public void SetGrounded(bool isGrounded) => animator.SetBool(groundedId, isGrounded);
         public void SetNormalizedSpeed(float speed) => animator.SetFloat(normalizedSpeedId, speed);
