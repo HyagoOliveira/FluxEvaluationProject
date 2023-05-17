ebf3388a098b5eefc6aee4786d32067f08e81b90
Hyago Oliveira <hyagogow@gmail.com>
2023-05-17 20:42:15 -0300

    Add AssetBundleLocalLoader component

---
+++ Assets/Scripts/AssetBundle/AssetBundleLocalLoader.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+
+namespace Flux.EvaluationProject
+{
+    public class AssetBundleLocalLoader : MonoBehaviour
+    {
+        [SerializeField] private string localPath;
+        [SerializeField] private string bundleName;
+        [SerializeField] private string bundleVariant;
+        [SerializeField] private string prefabName;
+        [SerializeField] private bool loadOnStart = true;
+
+        public event Action OnLoadCompleted;
+
+        private IEnumerator Start()
+        {
+            if (loadOnStart) yield return Load();
+        }
+
+        /// <summary>
+        /// Loads the bundle using the local fields.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator Load()
+        {
+            yield return InstantiateAsync(localPath, bundleName, bundleVariant, prefabName, transform);
+            OnLoadCompleted?.Invoke();
+        }
+
+        /// <summary>
+        /// Loads and instantiates a Prefab from a local asset bundle.
+        /// </summary>
+        /// <param name="localPath">The path where the local asset bundle is.</param>
+        /// <param name="name">The bundle name.</param>
+        /// <param name="variant">The bundle variant.</param>
+        /// <param name="prefabName">The prefab name inside the asset bundle.</param>
+        /// <param name="parent">A transform where the Prefab will be instantiated as a child.</param>
+        /// <returns></returns>
+        public static IEnumerator InstantiateAsync(
+            string localPath,
+            string name,
+            string variant,
+            string prefabName,
+            Transform parent
+        )
+        {
+            var bundleName = $"{name}.{variant}";
+            var path = Path.Combine(localPath, bundleName);
+            var bundleRequest = AssetBundle.LoadFromFileAsync(path);
+
+            yield return bundleRequest;
+
+            var bundle = bundleRequest.assetBundle;
+            if (bundle == null)
+            {
+                Debug.LogErrorFormat("Failed to load AssetBundle {0}", name);
+                yield break;
+            }
+
+            var prefabRequest = bundle.LoadAssetAsync<GameObject>(prefabName);
+            yield return prefabRequest;
+
+            var prefab = prefabRequest.asset as GameObject;
+            Instantiate(prefab, parent);
+
+            bundle.Unload(unloadAllLoadedObjects: false);
+        }
+    }
+}
